4ce10b6bc89369e7a604543caec1443335fd7e14
viniciusxavierPOA <jviniciusxavier26@gmail.com>
2021-03-30 19:36:00 -0500

    tela de login completa

--- src/pages/Signup/styles.ts
+++ src/pages/Signup/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import {shade} from 'polished';
 
-import signUpBackgroundImg from '../../assets/sign-up-background.png';
+import signUpBackgroundImg from '../../assets/backgroundCadastro.png';
 export const Container = styled.div`
 
     height:100vh;
